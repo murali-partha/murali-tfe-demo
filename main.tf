a8ad578e1138273477a2de16f32d03d72fc686c1
murali-partha <137029787+murali-partha@users.noreply.github.com>
2024-05-17 10:15:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,20 @@ provider "aws" {
   region = "us-east-1"  # Specify your desired AWS region
 }
 
+resource "time_sleep" "wait_60_seconds" {
+  depends_on = [null_resource.previous]
+
+  create_duration = "60s"
+}
+
+
+
 resource "aws_instance" "example" {
-  ami           = "ami-04e914639d0cca79a"  # Specify the AMI ID of the EC2 instance you want to deploy
+  ami           = "ami-0fe7e11d0d4fdbdbf"  # Specify the AMI ID of the EC2 instance you want to deploy
   instance_type = "t2.micro"      # Specify the instance type (e.g., t2.micro, t2.small, etc.)
   key_name      = "your_key_pair" # Specify the key pair name for SSH access
+  
+  depends_on = [time_sleep.wait_60_seconds]
 
   tags = {
     Name = "example-instance"  # Specify a name tag for the instance
